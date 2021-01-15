fc2fcd567041e0f9eb3970345b840371e72b35fe
Chun_Jonas Chan <JchanCode@gmail.com>
2021-01-15 14:47:02 -0500

    user model test

--- test_user_model.py
+++ test_user_model.py
@@ -7,7 +7,7 @@
 
 import os
 from unittest import TestCase
-
+from sqlalchemy import exc
 from models import db, User, Message, Follows
 
 # BEFORE we import our app, let's set an environmental variable
@@ -38,11 +38,11 @@ class UserModelTestCase(TestCase):
         db.drop_all()
         db.create_all()
 
-        u1 = User.signup("test1","test1@email.com","password",None)
+        u1 = User.signup("test1","test1@email.com","password",None,None,None,None)
         uid1 = 111
         u1.id = uid1
 
-        u2 = User.signup("test2","test2@email.com","password",None)
+        u2 = User.signup("test2","test2@email.com","password",None,None,None,None)
         uid2 = 222
         u2.id = uid2
 
@@ -113,14 +113,43 @@ class UserModelTestCase(TestCase):
         self.u1.following.append(self.u2)
         db.session.commit()
 
+        self.assertTrue(self.u1.is_following(self.u2))
+        self.assertFalse(self.u2.is_following(self.u1))
 
-        
+    def test_is_followed_by(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
 
-        def is_following(self, other_user):
-        """Is this user following `other_use`?"""
+        self.assertFalse(self.u1.is_followed_by(self.u2))
+        self.assertTrue(self.u2.is_followed_by(self.u1))
 
-        found_user_list = [
-            user for user in self.following if user == other_user]
-        return len(found_user_list) == 1
+    def test_signup(self):
+        tony = User.signup("tony", "tony@email.com", "password",
+                           None, None, None, None)
+        tid = 1234
+        tony.id = tid
+        db.session.commit()
 
+        tony = User.query.get(tid)
+        self.assertEqual(tony.username, "tony")
+        self.assertNotEqual(tony.password,"password")
+        self.assertTrue(tony.password.startswith("$2b$"))
 
+    def test_invalid_username(self):
+        invalid = User.signup(None,"test@test.com","password", None, None, None, None)
+        uid = 1122
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:  db.session.commit()
+
+    def test_invalid_password_signup(self):
+        with self.assertRaises(ValueError) as context: 
+            User.signup("test","test@email.com","",None,None,None,None)
+
+        with self.assertRaises(ValueError) as context:
+            User.signup("test","test@email.com",None,None,None,None,None)
+
+    def test_invalid_username_login(self):
+        self.assertFalse(User.authenticate("badusername","password"))   
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username,"badpassword"))
